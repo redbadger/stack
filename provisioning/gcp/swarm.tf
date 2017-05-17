10e2074c9236a87b15ca75e9ce478476f7cc3f21
Viktor Charypar <charypar@gmail.com>
2017-05-17 13:27:07 +0100

    Add provisioning of a worker autoscaling group

--- provisioning/gcp/swarm.tf
+++ provisioning/gcp/swarm.tf
@@ -1,37 +1,75 @@
 provider "google" {
-  project     = "microplatform-demo"
-  credentials = "${file("~/.config/gcloud/application_default_credentials.json")}"
+  project     = "${var.project_name}"
+  region      = "europe-west1"
+  credentials = "${file("credentials.json")}"
 }
 
-data "google_compute_network" "swarm" {
-  name = "microplatform-swarm"
+resource "google_compute_autoscaler" "swarm_workers" {
+  # TODO do this for each zone
+  name   = "swarm-workers"
+  zone   = "europe-west1-c"
+  target = "${google_compute_instance_group_manager.swarm_workers.self_link}"
+
+  autoscaling_policy {
+    min_replicas    = 3
+    max_replicas    = 6
+    cooldown_period = 30
+
+    cpu_utilization {
+      target = 0.5
+    }
+  }
 }
 
-resource "google_compute_instance" "test-node" {
-  machine_type = "f1-micro"
-  zone         = "europe-west1-c"
+resource "google_compute_instance_group_manager" "swarm_workers" {
+  name = "swarm-workers"
+  zone = "europe-west1-c" # TODO do this for each zone
 
-  metadata_startup_script = "${data.template_file.metadata_worker.rendered}"
+  instance_template  = "${google_compute_instance_template.swarm_worker.self_link}"
+  target_pools       = ["${google_compute_target_pool.swarm_workers.self_link}"]
+  base_instance_name = "swarm-worker"
+
+  depends_on = ["google_compute_instance_template.swarm_worker"]
+}
+
+resource "google_compute_instance_template" "swarm_worker" {
+  name_prefix    = "swarm-worker-"
+  machine_type   = "g1-small"
+  can_ip_forward = false
+  region         = "europe-west1"
+
+  metadata {
+    # user-data doesn't work on RancherOS on GCE
+    user-data = "${data.template_file.metadata_worker.rendered}"
+  }
 
   disk {
-    image = "rancheros-1-0-1"
+    source_image = "coreos-alpha-1409-0-0-v20170511"
   }
 
   network_interface {
-    network    = "demo"
-    subnetwork = "private"
+    subnetwork = "${var.subnetwork}"
 
     access_config {
       // Ephemeral IP
     }
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# load balancer target pool
+resource "google_compute_target_pool" "swarm_workers" {
+  name = "swarm-workers"
 }
 
 data "template_file" "metadata_worker" {
-  template = "${file("userdata/worker.yml")}"
+  template = "${file("cloud-init/coreos-worker.yml")}"
 
-  var {
-    MANAGER_IP = "127.0.9.1"
-    JOIN_TOKEN = "xxx"
+  vars {
+    MANAGER_IP = "${var.manager_ip}"
+    JOIN_TOKEN = "${var.worker_token}"
   }
 }
---
+++ provisioning/gcp/variables.tf
@@ -0,0 +1,17 @@
+variable "project_name" {
+  default = "312186932210"
+}
+
+variable "network" {
+  default = "demo"
+}
+
+variable "subnetwork" {
+  default = "private"
+}
+
+variable "manager_ip" {
+  default = "10.1.2.2"
+}
+
+variable "worker_token" {}
