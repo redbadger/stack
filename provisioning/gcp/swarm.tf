ccb6715b4ac156117098b5429a85fe42bf77df88
Viktor Charypar <charypar@gmail.com>
2017-05-18 18:20:26 +0100

    Automatically scale and join swarm managers

--- provisioning/gcp/swarm.tf
+++ provisioning/gcp/swarm.tf
@@ -23,8 +23,81 @@ resource "google_storage_bucket_acl" "swarm_state_acl" {
   bucket = "${google_storage_bucket.swarm_state.name}"
 
   role_entity = [
-    "READER:user-${google_service_account.swarm_state.email}",
+    "WRITER:user-${google_service_account.swarm_state.email}",
   ]
+}
+
+# Manager autoscaling group
+
+resource "google_compute_autoscaler" "swarm_managers" {
+  # TODO do this for each zone
+  name   = "swarm-managers"
+  zone   = "europe-west1-c"
+  target = "${google_compute_instance_group_manager.swarm_managers.self_link}"
+
+  autoscaling_policy {
+    min_replicas    = 3
+    max_replicas    = 3
+    cooldown_period = 30
+
+    cpu_utilization {
+      target = 0.5
+    }
+  }
+}
+
+resource "google_compute_instance_group_manager" "swarm_managers" {
+  name = "swarm-managers"
+  zone = "europe-west1-c" # TODO do this for each zone
+
+  instance_template  = "${google_compute_instance_template.swarm_manager.self_link}"
+  target_pools       = []
+  base_instance_name = "swarm-manager"
+
+  depends_on = ["google_compute_instance_template.swarm_manager"]
+}
+
+resource "google_compute_instance_template" "swarm_manager" {
+  name_prefix    = "swarm-manager-"
+  machine_type   = "g1-small"
+  can_ip_forward = false
+  region         = "europe-west1"
+
+  tags = ["docker-swarm-manager"]
+
+  service_account {
+    email  = "${google_service_account.swarm_state.email}"
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_write"]
+  }
+
+  metadata {
+    # user-data doesn't work on RancherOS on GCE
+    user-data = "${data.template_file.metadata_manager.rendered}"
+  }
+
+  disk {
+    source_image = "coreos-alpha-1409-0-0-v20170511"
+  }
+
+  network_interface {
+    subnetwork = "${var.subnetwork}"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+data "template_file" "metadata_manager" {
+  template = "${file("cloud-init/coreos-manager.yml")}"
+
+  vars {
+    BUCKET_NAME = "${google_storage_bucket.swarm_state.name}"
+  }
 }
 
 # Worker autoscaling group
