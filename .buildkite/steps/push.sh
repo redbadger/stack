9657cd519888ed1346cfce6384aff527f109b883
Stuart Harris <stuart.harris@red-badger.com>
2017-06-26 11:01:04 +0100

    push wip

--- .buildkite/steps/push.sh
+++ .buildkite/steps/push.sh
@@ -4,6 +4,11 @@ set -eux
 
 env
 
+# install gcloud
+# credentials.json
+# gcloud docker -a
+
+
 for file in "docker-compose-app.yml" "docker-compose-services.yml"; do
   docker-compose -f $file push
 done
