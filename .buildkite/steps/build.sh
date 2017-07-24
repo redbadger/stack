ce23e18cddd081ff701c029304aaaa26b8e1c9ad
Stuart Harris <stuart.harris@red-badger.com>
2017-07-24 10:51:33 +0100

    tmep fix CI

--- .buildkite/steps/build.sh
+++ .buildkite/steps/build.sh
@@ -5,8 +5,23 @@ set -eux
 env
 export registry=localhost:5000
 
-./provisioning/osx/configure/lib/index.js --file stacks.yml
+cat <<EOF >/tmp/app-ports.yml
+version: "3.1"
 
+services:
+  rproxy:
+    ports:
+      - 8001:3000
+EOF
+
+cat <<EOF >/tmp/services-ports.yml
+version: "3.1"
+
+services:
+  visualizer:
+    ports:
+      - 8000:3000
+EOF
 
 for stack in "app" "services"; do
   docker-compose -f ${stack}.yml -f /tmp/${stack}-ports.yml build
