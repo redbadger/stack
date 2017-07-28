653b907ff8a4e93d51334c29b11a434f5320e08c
Stuart Harris <stuart.harris@red-badger.com>
2017-07-28 16:56:46 +0100

    don't add health check if not specified

--- provisioning/osx/configure/src/haproxy.spec.js
+++ provisioning/osx/configure/src/haproxy.spec.js
@@ -26,4 +26,27 @@ describe('haproxy', () => {
     const actual = create(services, 'local');
     assert(actual === expected);
   });
+  it('should not add a health check if not specified', () => {
+    const services = [
+      {
+        stack: 'services',
+        name: 'visualizer',
+        aliases: [],
+        port: 8000,
+      },
+      {
+        stack: 'app',
+        name: 'rproxy',
+        health: '/status',
+        aliases: ['web'],
+        port: 8001,
+      },
+    ];
+    const expected = fs.readFileSync(
+      path.resolve(__dirname, '../fixtures/haproxy-no-health-check.cfg'),
+      'utf8',
+    );
+    const actual = create(services, 'local');
+    assert(actual === expected);
+  });
 });
