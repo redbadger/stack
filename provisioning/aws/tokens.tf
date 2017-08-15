81d5a5217c6000e70ff593060eab22f1bceb77e1
Stuart Harris <stuart.harris@red-badger.com>
2017-08-15 11:04:56 +0100

    efs filesystem and mount points

--- provisioning/aws/tokens.tf
+++ provisioning/aws/tokens.tf
@@ -1,48 +1,25 @@
-# resource "aws_vpc_endpoint" "private_s3" {
-#   vpc_id       = "${var.vpc_id}"
-#   service_name = "com.amazonaws.${var.region}.s3"
-# }
-#
-# data "aws_iam_policy_document" "s3_through_endpoint" {
-#   statement {
-#     sid     = "Access-to-specific-VPCE-only"
-#     effect  = "Deny"
-#     actions = ["s3:*"]
-#
-#     resources = [
-#       "arn:aws:s3:::${aws_s3_bucket.tokens.id}",
-#       "arn:aws:s3:::${aws_s3_bucket.tokens.id}/*",
-#     ]
-#
-#     condition {
-#       test     = "StringNotEquals"
-#       variable = "aws:sourceVpce"
-#       values   = ["${aws_vpc_endpoint.private_s3.id}"]
-#     }
-#
-#     principals {
-#       type        = "AWS"
-#       identifiers = ["*"]
-#     }
-#   }
-# }
-# resource "aws_vpc_endpoint_route_table_association" "private_s3" {
-#   vpc_endpoint_id = "${aws_vpc_endpoint.private_s3.id}"
-#   route_table_id  = "${aws_route_table.private_s3.id}"
-# }
-#
-# resource "aws_s3_bucket_policy" "s3_through_endpoint" {
-#   bucket = "${aws_s3_bucket.tokens.id}"
-#   policy = "${data.aws_iam_policy_document.s3_through_endpoint.json}"
-# }
-#
-# resource "aws_s3_bucket" "tokens" {
-#   bucket        = "microplatform.tokens"
-#   acl           = "private"
-#   force_destroy = true
-#
-#   tags {
-#     Name = "Tokens"
-#   }
-# }
+resource "aws_efs_file_system" "tokens" {
+  tags {
+    Name = "Tokens"
+  }
+}
 
+resource "aws_efs_mount_target" "tokens" {
+  count           = "${length(var.private_subnets)}"
+  file_system_id  = "${aws_efs_file_system.tokens.id}"
+  subnet_id       = "${element(var.private_subnets, count.index)}"
+  security_groups = ["${aws_security_group.efs_mount_target.id}"]
+}
+
+resource "aws_security_group" "efs_mount_target" {
+  name        = "efs_mount_target"
+  description = "For EFS Mount targets"
+  vpc_id      = "${var.vpc_id}"
+
+  ingress {
+    from_port       = 2049
+    to_port         = 2049
+    protocol        = "tcp"
+    security_groups = ["${aws_security_group.nodes.id}"]
+  }
+}
