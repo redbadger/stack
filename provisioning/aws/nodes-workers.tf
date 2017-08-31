86cbad5239d9bee1893a58e0740480aa91b364c6
Stuart Harris <stuart.harris@red-badger.com>
2017-08-31 16:47:44 +0100

    allow ports 8000 to 9000 on swarm

--- provisioning/aws/nodes-workers.tf
+++ provisioning/aws/nodes-workers.tf
@@ -18,8 +18,8 @@ resource "aws_security_group" "web_servers" {
   vpc_id      = "${var.vpc_id}"
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8000
+    to_port     = 9000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
