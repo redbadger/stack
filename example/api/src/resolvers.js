a12ce6817c4d8f2ab3e77f42de60205212b5e238
Stuart Harris <stuart.harris@red-badger.com>
2017-11-16 10:55:51 +0000

    improve secrets handling

--- example/api/src/resolvers.js
+++ example/api/src/resolvers.js
@@ -18,10 +18,18 @@ export default {
       } catch (e) {
         files = [];
       }
-      return files.map(async file => ({
-        name: file,
-        value: await readFile(Path.resolve(path, file), 'utf8'),
-      }));
+      // even though '.' and '..' are not included, K8s adds extra private files
+      // to mount point, so filter first
+      return files.filter(name => !name.startsWith('.')).map(async name => {
+        try {
+          return {
+            name,
+            value: await readFile(Path.resolve(path, name), 'utf8'),
+          };
+        } catch (e) {
+          return { name };
+        }
+      });
     },
     headers(_1, _2, req) {
       return JSON.stringify(req && req.headers);
